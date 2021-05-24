b195745f126e445faeb4a559fcc4bb22d40f8cae
Ksylich <andrey.okselenko@computools.com>
2021-05-24 15:58:17 +0300

    First commit

---
+++ src/Dogs/index.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { gql, useQuery, NetworkStatus } from "@apollo/client";
+
+const GET_DOGS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
+
+const GET_DOG_PHOTO = gql`
+  query Dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+const Dogs = ({ onDogSelected }) => {
+  const { loading, error, data } = useQuery(GET_DOGS);
+
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>Error !!! :(</p>;
+
+  return (
+    <select name="dog" onChange={onDogSelected}>
+      {data.dogs.map((dog) => (
+        <option key={dog.id} value={dog.breed}>
+          {dog.breed}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+const DogPhoto = ({ breed }) => {
+  const { loading, error, data, refetch, networkStatus } = useQuery(GET_DOG_PHOTO, {
+    variables: { breed },
+    notifyOnNetworkStatusChange: true,
+    // pollInterval: 500,
+  });
+
+  if (networkStatus === NetworkStatus.refetch) return 'Refetching!';
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
+
+  return (
+    <div>
+      <img
+        src={data.dog.displayImage}
+        style={{ height: 200, width: 200, margin: "2rem" }}
+      />
+      <button onClick={() => refetch()}>Refetch!</button>
+    </div>
+  );
+};
+
+const DisplayDog = () => {
+  const [selectedDog, setSelectedDog] = useState(null);
+
+  const onDogSelected = ({ target }) => {
+    setSelectedDog(target.value);
+  };
+
+  return (
+    <>
+      <Dogs onDogSelected={onDogSelected} />
+      {selectedDog && <DogPhoto breed={selectedDog} />}
+    </>
+  );
+};
+
+export default DisplayDog;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client/react';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://71z1g.sse.codesandbox.io/',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
